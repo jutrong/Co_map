2c08c2780345dc92e52a667b9231adb9e7b2c418
[SW5] 이주현 <leek71358@gmail.com>
2023-07-20 00:21:08 +0900

    123

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -4,7 +4,7 @@ import UsersList from "../components/UserList";
 const Users = () => {
   const USERS = [
     {
-      id: "u5",
+      id: "u6",
       name: "jutrong",
       image:
         "https://express-images.franklymedia.com/6616/sites/380/2021/01/08114610/the-rock-youtube.jpg",
