844c5ab07171aa043dff491f3ed1931118798c50
[SW5] 이주현 <leek71358@gmail.com>
2023-07-19 23:55:50 +0900

    check

---
+++ src/user/pages/Users.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import UsersList from "../components/UserList";
+
+const Users = () => {
+  const USERS = [
+    {
+      id: "u2",
+      name: "jutrong",
+      image:
+        "https://express-images.franklymedia.com/6616/sites/380/2021/01/08114610/the-rock-youtube.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList />;
+};
+
+export default Users;
