7afd9bac67fbe74dbf5754cf86ce64e3606ff1ef
[SW5] 이주현 <leek71358@gmail.com>
2023-07-20 00:09:57 +0900

    new commit

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -4,7 +4,7 @@ import UsersList from "../components/UserList";
 const Users = () => {
   const USERS = [
     {
-      id: "u3",
+      id: "u4",
       name: "jutrong",
       image:
         "https://express-images.franklymedia.com/6616/sites/380/2021/01/08114610/the-rock-youtube.jpg",
