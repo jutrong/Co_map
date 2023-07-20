a3dceaad7b56b371889bb559424f0d82696d3bb0
[SW5] 이주현 <leek71358@gmail.com>
2023-07-20 22:58:02 +0900

    메인 헤더 추가

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import UsersList from "../components/UserList";
+import UsersList from "../pages/components/UsersList";
 
 const Users = () => {
   const USERS = [
     {
-      id: "u7",
+      id: "u1",
       name: "jutrong",
       image:
         "https://express-images.franklymedia.com/6616/sites/380/2021/01/08114610/the-rock-youtube.jpg",
       places: 3,
     },
   ];
-  return <UsersList />;
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
